0107899405dbd95596e570231319d980b22f452b
Artem Konev <artem.konev@canonical.com>
2025-05-30 13:15:32 +0100

    Added LICENSE and license comments.

--- gencodo.go
+++ gencodo.go
@@ -1,3 +1,21 @@
+// This file is part of gencodo, a library for generating Go template based docs from cobra CLI applications
+//
+// Copyright 2025 Canonical Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-only
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License version 3,
+// as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranties of
+// MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see http://www.gnu.org/licenses/.
+
 package gencodo
 
 import (
